4e341e8c6bb3def84c48bb88c40506e4fad15b53
Weerayut mayusiri <aae606@gmail.com>
2020-06-20 19:34:42 +0700

    ok ake

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,62 +7,69 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-  psy: any = [];
+  psyApi = 'https://sheet.best/api/sheets/b7a2ce1d-d82e-4475-b394-85e6c5dc90ab';
   dataPsy: any = [];
-  dataPsy2: any = [];
-  dataPsy3: any = [];
-
+  dataPsy2: Array<string> = [];
+  dataPsy3: Array<number> = [];
+  Highcharts: typeof Highcharts = Highcharts; // required
+  chartOptions: Highcharts.Options = {
+    series: [
+      {
+        type: 'column',
+        data: []
+      },
+    ],
+  }; // required
   constructor(private http: HttpClient) { }
 
-highcharts = Highcharts;
-  chartOptions = {
-    chart: {
-      type: 'column'
-   },
-   title: {
-      text: 'Prasri Kpi 2020'
-   },      xAxis: {
-         categories: `${this.dataPsy2}`
-        //  categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
-        //  'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        //  crosshair: true
-      },
-      yAxis : {
-         min: 0,
-         title: {
-            text: 'จำนวน (ราย)'
-         }
-      },
-      plotOptions : {
-         column: {
-            pointPadding: 0.2,
-            borderWidth: 0
-         }
-      },
-      series: [{
-         name: 'เขตสุขภาพ',
-         data: this.dataPsy3
-        //  data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6,
-        //     148.5, 216.4, 194.1, 95.6, 54.4]
-      }]
-  };
 
-  psyApi = 'https://sheet.best/api/sheets/b7a2ce1d-d82e-4475-b394-85e6c5dc90ab';
+
+
   async ngOnInit() {
-    // await this.getPsy();
-    // alert( this.dataPsy2);
-    // alert( this.dataPsy3);
+
+
+      const req = await this.http.get(this.psyApi).toPromise();
+      this.dataPsy = req;
+      this.dataPsy2 = this.dataPsy.map( (data) => {
+        return data.ahb;
+      });
+      this.dataPsy3 = this.dataPsy.map( (data) => {
+        return parseFloat( data.percen );
+      });
+      console.log(this.dataPsy2);
+      console.log(this.dataPsy3);
+
+
+      this.chartOptions = {
+        title: {
+          text: 'Prasri Kpi 2020',
+        },
+        xAxis: {
+          categories: this.dataPsy2,
+
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: 'จำนวน (ราย)',
+          },
+        },
+        plotOptions: {
+          column: {
+            pointPadding: 0.2,
+            borderWidth: 0,
+          },
+        },
+        series: [
+          {
+            name: 'เขตสุขภาพ',
+            type: 'column',
+            data: this.dataPsy3,
+            // data:  [94.46, 83.72, 89.25, 114.19, 65.54, 69.85, 127.43, 114.76, 97.96, 124.41, 91.59, 89.1, 15.9]
+          },
+        ],
+      };
+
   }
-  async getPsy() {
-    const req = await this.http.get(this.psyApi).toPromise();
-    this.dataPsy = req;
-    this.dataPsy2 = this.dataPsy.map( (data) => {
-      return data.ahb;
-    });
-    this.dataPsy3 = this.dataPsy.map( (data) => {
-      return parseFloat( data.percen );
-    });
-    console.log(this.dataPsy2);
-    console.log(this.dataPsy3);
-  }
+
 }
